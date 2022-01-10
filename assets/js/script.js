2e838f0df5ec7a99d044bed70fa5b53e3adbba85
wyork <w.york63@gmail.com>
2022-01-10 18:21:21 -0500

    added comments on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var formSubmitHandler = function(event) {
         getLatLon(city);
         cityInputEl.value = '';
     } else {
-        //unhides message if not working
+        //unhides message if not working - new 1/9 
         alertBox.classList = 'unhide'
         hideDrink.classList = 'hide'
     }
@@ -36,7 +36,7 @@ var getLatLon = function(city) {
                     var lon = data.coord.lon;
                     getAir(lat, lon, city);
                     console.log(lat, lon);
-                    // triggers on research to hide alert again and display drink if it was hidden
+                    // triggers on research to hide alert again and display drink if it was hidden new1 1-9
                     alertBox.classList = 'hide'
                     hideDrink.classList = 'unhide'
                 });
@@ -59,9 +59,7 @@ var getAir = function(lat, lon, city) {
     var apiKey2 = '54cd8f0a-9f51-43d4-ad67-cb829c5298e2';
     var apiUrl2 = 'https://api.airvisual.com/v2/nearest_city?lat=' + lat + '&lon=' + lon + '&key=' + apiKey2;
     fetch(apiUrl2)
-
-    //option 1 pt 1 
-    // below code returns success and city name but posts undefined 
+    // code to return the air quality 
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(apiresponse) {
@@ -74,26 +72,24 @@ var getAir = function(lat, lon, city) {
 
     var displayAir = function(apiresponse, city) {
         console.log((apiresponse));
-
-        //     //clear results
+        //clear results
         resultsContainerEl.textContent = ''
 
-        //     // new container for info
+        // new container for info
         var currentAirEl = document.createElement('article');
         currentAirEl.id = 'current';
         currentAirEl.classList = ''
 
-        //     // information to add to this div
+        // information to add to this div
         var airQualityEl = document.createElement('p')
 
-        //     // the info from the data here should show up but is showing up as undefined
-        //     // data itself is showing up as [object Object]
-        //     // means that it is not being parsed from JSON appropriately - but where is it going wrong? 
+        //pulls data from api and pushes it into html
         airQualityEl.innerHTML = 'Right now the AQI in ' + city + ' is ' + apiresponse.data.current.pollution.aqius;
 
-        //     //append there to currentAir section
+        //append there to currentAir section
         currentAirEl.appendChild(airQualityEl);
 
+        // function to change color of result depending on level of airquality
         resultsContainerEl.appendChild(currentAirEl);
         var airColorEl = document.querySelector('#results');
         if (parseInt(apiresponse.data.current.pollution.aqius) < 50) {
@@ -105,6 +101,5 @@ var getAir = function(lat, lon, city) {
         }
     }
 }
-
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
