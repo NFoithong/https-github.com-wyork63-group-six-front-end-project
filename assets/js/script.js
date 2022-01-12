be4fcb1da385a72ba78ad9d216b83e88f968f2d5
NFoithong <n.foithong1983@gmail.com>
2022-01-12 08:53:20 -0500

    responsive and modal styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var alertBox = document.querySelector('#alert');
 // var hideDrink = document.querySelector('#drink');
 var cities = []
 
+
 // when search button is clicked - it finds input for that location
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -17,12 +18,13 @@ var formSubmitHandler = function(event) {
     } else {
         //unhides message if not working - new 1/9 
         alertBox.classList = 'unhide'
-        // hideDrink.classList = 'hide'
+            // hideDrink.classList = 'hide'
+
     }
     saveSearch();
 };
-    var saveSearch = function () {
-        localStorage.setItem("cities", JSON.stringify(cities));
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
 };
 
 
@@ -43,13 +45,13 @@ var getLatLon = function(city) {
                     getAir(lat, lon, city);
                     // triggers on research to hide alert again and display drink if it was hidden new1 1-9
                     alertBox.classList = 'hide'
-                    // hideDrink.classList = 'unhide'
+                        // hideDrink.classList = 'unhide'
                 });
             } else {
                 // unhides alert and makes sure drink doesnt pop up
                 alertBox.classList = 'unhide';
                 // hideDrink.classList = 'hide'
-                
+
             }
         })
         .catch(function(error) {
@@ -63,16 +65,16 @@ var getAir = function(lat, lon, city) {
     var apiKey2 = '54cd8f0a-9f51-43d4-ad67-cb829c5298e2';
     var apiUrl2 = 'https://api.airvisual.com/v2/nearest_city?lat=' + lat + '&lon=' + lon + '&key=' + apiKey2;
     fetch(apiUrl2)
-    // code to return the air quality 
-    .then(function(response) {
-        if (response.ok) {
-            response.json().then(function(apiresponse) {
-                displayAir(apiresponse, city)
-            });
-        } else {
-            alert("Error: Could not Connect. Please check spelling and try again")
-        }
-    });
+        // code to return the air quality 
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(apiresponse) {
+                    displayAir(apiresponse, city)
+                });
+            } else {
+                alert("Error: Could not Connect. Please check spelling and try again")
+            }
+        });
 
     var displayAir = function(apiresponse, city) {
 
@@ -91,22 +93,25 @@ var getAir = function(lat, lon, city) {
         //append there to currentAir section
         currentAirEl.appendChild(airQualityEl);
 
-        // create new div-class time
         var divTime = document.createElement('div');
         divTime.className = 'time';
         divTime.innerHTML = '';
 
         currentAirEl.appendChild(divTime);
 
-        // create h2-date
-        var date = document.createElement('h2');
+        var cityName = document.createElement('h2');
+        cityName.className = 'curCity';
+        cityName.innerHTML = city;
+
+        divTime.appendChild(cityName);
+
+        var date = document.createElement('p');
         var todayDate = moment().format('dddd MMMM Do YYYY');
         date.className = 'currentDate';
         date.innerHTML = todayDate;
 
         divTime.appendChild(date);
 
-        // create p-current time
         var todayTime = document.createElement('p');
         var curTime = moment().format('LTS');
         todayTime.className = 'currentTime';
@@ -114,50 +119,70 @@ var getAir = function(lat, lon, city) {
 
         divTime.appendChild(todayTime);
 
-        // create p-city
-        var cityName = document.createElement('p');
-        cityName.className = 'curCity';
-        cityName.innerHTML = city;
+        var uvIndex = document.createElement('div');
+        uvIndex.className = 'uvIndex';
+        uvIndex.innerHTML = '';
 
-        divTime.appendChild(cityName);
+        currentAirEl.appendChild(uvIndex);
 
-        // create div-class aqiData
+        var indexDesc = document.createElement('p');
+        indexDesc.className = 'desc';
+        // indexDesc.innerHTML = '';
+
+        uvIndex.appendChild(indexDesc);
+
         var aqiData = document.createElement('div');
         aqiData.className = 'aqi';
         aqiData.innerHTML = '';
 
         currentAirEl.appendChild(aqiData);
 
-        // create h2-data
         var dataNumber = document.createElement('h2');
         dataNumber.className = 'aqiData';
         dataNumber.innerHTML = apiresponse.data.current.pollution.aqius;
 
         aqiData.appendChild(dataNumber);
 
-        // create p-aqi
         var usAqi = document.createElement('span');
         usAqi.className = 'aqi-us';
         usAqi.innerHTML = 'AQI';
 
         aqiData.appendChild(usAqi);
 
-
-
-        
         // function to change color of result depending on level of airquality
         resultsContainerEl.appendChild(currentAirEl);
         var airColorEl = document.querySelector('.aqi');
+
+
         if (parseInt(apiresponse.data.current.pollution.aqius) < 50) {
             airColorEl.classList.add('good');
+            indexDesc.innerHTML = "This AQI is <b>GOOD</b>! <br>Let's go out for party!";
 
         } else
         if (parseInt(apiresponse.data.current.pollution.aqius) > 150) {
             airColorEl.classList.add('unhealthy');
+            indexDesc.innerHTML = "This AQI is <b>UNHEALTHY</b>! <br>Stay home and relax with your homemade drink!.";
+
         } else {
             airColorEl.classList.add('moderate');
+            indexDesc.innerHTML = "This AQI is <b>MODERATE</b>! <br>Considering before going out!";
+
         }
     }
+}
+
+// when Search click  -> dashboard height.style.change
+function fun() {
+
+    var dashBoard = document.querySelector("#dashBoard");
+    if (dashBoard.style.height = "100vh") {
+        h = dashBoard.style.height;
+        dashBoard.style.height = "auto";
+    } else {
+        dashBoard.style.height = h;
+    }
+    // return false;
+
 }
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
--- assets/js/time.js
+++
@@ -1,8 +0,0 @@
-// Create new div element and add current date/time
-var colDashboard = document.querySelector('.col1');
-var today = moment().format('llll');
-var div = document.createElement('div');
-div.className = 'currenTime';
-// console.log(today);
-div.innerHTML = today;
-colDashboard.appendChild(div);
